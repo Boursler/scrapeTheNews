2e28f3cc9b75bcd562a7710c3db6830f93b9908a
Briana Oursler <briana.oursler@gmail.com>
2018-06-08 18:23:07 -0700

    Initial handlebars

--- controller/controller.js
+++ controller/controller.js
@@ -52,7 +52,7 @@ module.exports = function (app) {
 		db.Article.find({})
 			.then(function (dbArticle) {
 				// If we were able to successfully find Articles, send them back to the client
-				res.json(dbArticle);
+				res.render("index", dbArticle);
 			})
 			.catch(function (err) {
 				// If an error occurred, send it to the client
