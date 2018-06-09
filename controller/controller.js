1bd5d9f12daa41d2e764e69164061d368265d52f
Briana Oursler <briana.oursler@gmail.com>
2018-06-08 19:45:02 -0700

    Fix data handling to /articles GET

--- controller/controller.js
+++ controller/controller.js
@@ -52,7 +52,10 @@ module.exports = function (app) {
 		db.Article.find({})
 			.then(function (dbArticle) {
 				// If we were able to successfully find Articles, send them back to the client
-				res.render("index", dbArticle);
+				var hbsObject = {
+					article: dbArticle
+				}
+				res.render("index", hbsObject);
 			})
 			.catch(function (err) {
 				// If an error occurred, send it to the client
